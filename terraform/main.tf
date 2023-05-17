af63dbf5b57323beeaa3766d7dae1e2c1922a235
Sachith Senarathne <sachith.senarathne@zuhlke.com>
2023-05-17 12:16:46 +0800

    Adding secret and key_id to aws provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,8 @@ provider "kubernetes" {
 
 provider "aws" {
   region = var.region
+  access_key = var.access_key_id
+  secret_key = var.secret_access_key
 }
 
 data "aws_availability_zones" "available" {}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   type = string
   default = "ap-southeast-1"
 }
+
+variable "access_key_id" {
+  description = "AWS ACCESS_KEY_ID"
+  type = string
+  default = "AKIA47ZYZ3ERVTYTA34K"
+}
+
+variable "secret_access_key" {
+  description = "AWS SECRET_ACCESS_KEY"
+  type = string
+  default = "EGDkMPzJp/zKKjvk5Z2g9GZY7kJrIsSQjdLZ+olM"
+}
