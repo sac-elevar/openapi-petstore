0dc2dff0f6545fb5249f1f3cda01e063ad2913d7
Sachith Senarathne <sachith.senarathne@zuhlke.com>
2023-05-17 17:44:49 +0800

    Removing unsafe keys

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,11 +7,9 @@ variable "region" {
 variable "access_key_id" {
   description = "AWS ACCESS_KEY_ID"
   type = string
-  default = "AKIA47ZYZ3ERVTYTA34K"
 }
 
 variable "secret_access_key" {
   description = "AWS SECRET_ACCESS_KEY"
   type = string
-  default = "EGDkMPzJp/zKKjvk5Z2g9GZY7kJrIsSQjdLZ+olM"
 }
