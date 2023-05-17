092f63928c08324c36704eadb05227bd783b3537
Sachith Senarathne <sachith.senarathne@zuhlke.com>
2023-05-17 18:56:57 +0800

    Removing unwanted vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,15 +3,3 @@ variable "region" {
   type = string
   default = "ap-southeast-1"
 }
-
-variable "aws_access_key_id" {
-  description = "AWS ACCESS_KEY_ID"
-  type = string
-  default = ""
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS SECRET_ACCESS_KEY"
-  type = string
-  default = ""
-}
